42bcfc3de8499d44ec1739d96412cfe2b9f3c80f
kcirekcom <erick.f.mock@gmail.com>
2016-11-14 11:17:02 -0800

    adding x and y scales to bar chart, determing xAxis and yAxis positioning

--- scripts/d3graphs.js
+++ scripts/d3graphs.js
@@ -3,3 +3,17 @@
 var margin = {top: 20, right: 30, bottom: 30, left: 40},
   width = 960 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+  .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+  .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+  .scale(x)
+  .orient('bottom');
+
+var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient('left');
