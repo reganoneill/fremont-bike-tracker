f48054d9c064fd0a77d727ddee80951c40969220
kcirekcom <erick.f.mock@gmail.com>
2016-11-14 16:31:38 -0800

    refactored the D3 chart to work with the recent version 4.3instead of version 3

--- scripts/d3graphs.js
+++ scripts/d3graphs.js
@@ -1,30 +1,28 @@
 'use strict';
 
 var margin = {top: 20, right: 30, bottom: 30, left: 40},
-  width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+  width = 420 - margin.left - margin.right,
+  height = 300 - margin.top - margin.bottom;
 
-var x = d3.scale.ordinal()
-  .rangeRoundBands([0, width], .1);
+var x = d3.scaleBand()
+  .range([0, width])
+  .padding(0.25);
 
-var y = d3.scale.linear()
+var y = d3.scaleLinear()
   .range([height, 0]);
 
-var xAxis = d3.svg.axis()
-  .scale(x)
-  .orient('bottom');
-
-var yAxis = d3.svg.axis()
-  .scale(y)
-  .orient('left');
-
-var chart = d3.select('.chart')
+var svg = d3.select('body').append('svg')
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom)
   .append('g')
   .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-d3.json('', type, function(error, data) {
+d3.json('../../example.json', function(error, data) {
+  console.log(data);
+  data.forEach(function(d) {
+    d.value = +d.value;
+  });
+
   x.domain(data.map(function(d) {
     return d.name;
   }));
@@ -32,4 +30,19 @@ d3.json('', type, function(error, data) {
     return d.value;
   })]);
 
+  svg.append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(x));
+
+  svg.append('g')
+    .call(d3.axisLeft(y));
+
+  svg.selectAll('.bar')
+    .data(data)
+    .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', function(d) { return x(d.name); })
+    .attr('width', x.bandwidth())
+    .attr('y', function(d) { return y(d.value); })
+    .attr('height', function(d) { return height - y(d.value); });
 });
